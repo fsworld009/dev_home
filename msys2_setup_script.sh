0c7c5a4c9806a21b3bd9ebc422b5f31b38754991
WorldFS <fsworld009@gmail.com>
2015-05-17 21:14:54 -0400

    import vimrc from msysgit, use gvim and disable vim from msys2 (too buggy)

--- msys2_setup_script.sh
+++ msys2_setup_script.sh
@@ -8,3 +8,7 @@ pause 'Press [Enter] key to continue...'
 
 #http://stackoverflow.com/questions/3778042/github-error-cloning-my-private-repository
 git config --system http.sslcainfo "/etc/git/curl-ca-bundle.crt"
+
+#disable the vim come from msys2, use vim from gvim instead ($PATH was setup by sh.bat)
+cd /usr/bin/
+mv vim.exe vim2.exe
--- sh.bat
+++ sh.bat
@@ -5,4 +5,6 @@ rem get parent folder
 rem http://stackoverflow.com/questions/16623780/how-to-get-windows-batchs-parent-folder
 for %%i in ("%~dp0..") do set "folder=%%~fi"
 
+set PATH=%PATH%;%folder%\gvim74\
+
 %folder%\msys64\usr\bin\sh.exe -c 'MSYSTEM=MINGW64 exec /bin/sh --login -i'
