5b9ee2bd9a90789ac69068465f7753a131a0ebf4
WorldFS <fsworld009@gmail.com>
2015-07-07 00:52:05 -0400

    update readme, npm packages

--- iojs/npm_install.sh
+++ iojs/npm_install.sh
@@ -6,4 +6,8 @@ cd ../../bin/iojs/
 ./npm install csslint -g
 ./npm install tern -g
 ./npm install jshint -g
+./npm install jquery -g
+./npm install underscore -g
+./npm install hexo-cli -g
+./npm install electron-prebuilt -g
 ./npm install -g git://github.com/ramitos/jsctags.git
