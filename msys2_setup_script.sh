3eb91a977cbfac73bfcb94b7cffdccfd35d86e66
WorldFS <fsworld009@gmail.com>
2015-09-20 19:38:24 -0400

    script to disable msys2 vim and python

--- msys2_setup_script.sh
+++ msys2_setup_script.sh
@@ -14,11 +14,6 @@ pause 'Press [Enter] key to continue...'
 #http://stackoverflow.com/questions/3778042/github-error-cloning-my-private-repository
 #git config --system http.sslcainfo "/etc/git/curl-ca-bundle.crt"
 
-#disable the vim come from msys2, use vim from gvim instead ($PATH was setup by sh.bat)
-cd /usr/bin/
-mv vim.exe vim2.exe
-pause 'Press [Enter] key to continue...'
-
 #toolchain for mingw
 pacman -S mingw-w64-x86_64-toolchain --noconfirm
 pause 'Press [Enter] key to continue...'
@@ -38,4 +33,20 @@ makepkg-mingw
 #automatically done everything now?
 #pacman -U mingw-w64-x86_64-tcl-8.6.4-1-any.pkg.tar.xz
 pause 'Press [Enter] key to continue...'
+
+
+#disable some binaries come from msys2, use vim from gvim instead ($PATH was setup by sh.bat)
+echo 'disable vim & python from msys2'
+cd /usr/bin/
+mv vim.exe vim.exe_
+cd /msys64/mingw64/bin
+mv python.exe python.exe_
+mv python2.exe python2.exe_
+mv python2.7.exe python2.7.exe_
+mv python2_ python2_
+pause 'Press [Enter] key to continue...'
+
+
+
+
 cd ~
